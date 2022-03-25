670ac3dbec92bb4745c79072799ac3a8cc80b176
Moamin <moamin.salamah@gmail.com>
2022-03-25 13:29:07 +0100

    full working responsive dashboard

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+    {
+        productName:'Flodable Mini Drone',
+        productNumber:'23746',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'LARVENDER KF102 Drone',
+        productNumber:'26546',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+    {
+        productName:'ROKO Drone',
+        productNumber:'46532',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'SHARK Drone',
+        productNumber:'24578',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'DARKNIGHT Drone',
+        productNumber:'2366',
+        paymentStatus:'Due',
+        shipping:'Pending'
+    },
+    {
+        productName:'Flodable Mini Drone',
+        productNumber:'54746',
+        paymentStatus:'Paid',
+        shipping:'Delivered'
+    },
+    {
+        productName:'FLY FLY Drone',
+        productNumber:'98732',
+        paymentStatus:'Refunded',
+        shipping:'Declined'
+    },
+];
